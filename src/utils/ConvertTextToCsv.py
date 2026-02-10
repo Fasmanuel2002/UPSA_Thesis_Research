f3257d650b3d2ddc946e389d537f3b7f770654d8
ManuelFas <manuelfasruiz@gmail.com>
2026-02-10 16:51:24 +0100

    Added commentaries

--- src/utils/ConvertTextToCsv.py
+++ src/utils/ConvertTextToCsv.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def TextToCsv(file_path : str) -> pd.DataFrame:
+    """
+    Function for making text file to CSV format
+    """
     df = pd.read_csv(file_path, sep="\\t", engine="python")
     print(f"Shape of the CSV: {df.shape}")
     return df
