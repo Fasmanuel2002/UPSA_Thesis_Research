4b285d864ce1517288096c360c12c89b3b31b195
ManuelFas <manuelfasruiz@gmail.com>
2026-02-20 14:11:06 +0100

    Added try catch and PCA for 2 vatriables

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -2,16 +2,29 @@ import pandas as pd
 import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
-
+from sklearn.preprocessing import StandardScaler
+import plotly.express as px
+from sklearn.decomposition import PCA
 
 def box_plot(df : pd.DataFrame, title : str, type_cancer : str) -> None:
     """
     Function to make box plots
     """
-    if type_cancer == "Luminal A":
-        df = df[df["Tumor-Cancer"] == "Luminal A"].iloc[:, 1:]
-    elif type_cancer == "Luminal B":
-        df = df[df["Tumor-Cancer"] == "Luminal B"].iloc[:, 1:]
+    try:
+        if type_cancer == "Luminal A":
+            df = df[df["Tumor-Cancer"] == "Luminal A"].iloc[:, 1:]
+            
+        elif type_cancer == "Luminal B":
+            df = df[df["Tumor-Cancer"] == "Luminal B"].iloc[:, 1:]
+            
+        elif type_cancer == "TNBC":
+            df = df[df["Tumor-Cancer"] == "TNBC"].iloc[:, 1:]
+            
+        elif type_cancer == "HER2-enriched":
+            df = df[df["Tumor-Cancer"] == "HER2-enriched"].iloc[:, 1:]
+    except TypeError:
+        print(f"Error, you need to put the values of type cancer like this Luminal A, Luminal B, TNBC and HER2-enriched")
+    
     
     plt.figure(figsize=(12,5))
     plt.boxplot(np.log2(df[1:21].T + 1))  # transpose
@@ -21,10 +34,21 @@ def box_plot(df : pd.DataFrame, title : str, type_cancer : str) -> None:
     
 
 def histogram_log2(df: pd.DataFrame, title: str, type_cancer : str) -> None:
-    if type_cancer == "Luminal A":
-        df = df[df["Tumor-Cancer"] == "Luminal A"].iloc[:, 1:]
-    elif type_cancer == "Luminal B":
-        df = df[df["Tumor-Cancer"] == "Luminal B"].iloc[:, 1:]
+    try:
+        if type_cancer == "Luminal A":
+            df = df[df["Tumor-Cancer"] == "Luminal A"].iloc[:, 1:]
+            
+        elif type_cancer == "Luminal B":
+            df = df[df["Tumor-Cancer"] == "Luminal B"].iloc[:, 1:]
+            
+        elif type_cancer == "TNBC":
+            df = df[df["Tumor-Cancer"] == "TNBC"].iloc[:, 1:]
+            
+        elif type_cancer == "HER2-enriched":
+            df = df[df["Tumor-Cancer"] == "HER2-enriched"].iloc[:, 1:]
+    except ValueError:
+        print(f"Error, you need to put the values of type cancer like this Luminal A, Luminal B, TNBC and HER2-enriched")
+        
         
     x = np.log2(df.iloc[1:20000].T.values.flatten() + 1)
     counts, bins = np.histogram(x, bins=100)
@@ -56,3 +80,56 @@ def histogram_log2(df: pd.DataFrame, title: str, type_cancer : str) -> None:
     plt.tight_layout()
 
     plt.show()
+    
+
+def PCA_2_variables(df : pd.DataFrame,  cancer_type_one : str, cancer_type_two : str) -> None:
+    """
+    Function to make the plot to compare only two type of cancer mamals
+    Types:
+        - Luminal A
+        - Luminal B
+        - Triple Negative (TNBC)
+        - HER-enriched 
+    """
+    y = df["Tumor-Cancer"]
+    X = df.drop(columns=["Tumor-Cancer"])
+
+
+    X = X.apply(pd.to_numeric, errors="coerce").fillna(0)
+
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    pca = PCA(n_components=2, random_state=0)
+    components = pca.fit_transform(X_scaled)
+
+
+    df_plot = pd.DataFrame(components, columns=["PC1", "PC2"])
+    df_plot["Tumor-Cancer"] = y.values
+    df_plot["Patient_ID"] = df.index
+
+    labels = {
+        "PC1": f"PC1 ({pca.explained_variance_ratio_[0]*100:.1f}%)",
+        "PC2": f"PC2 ({pca.explained_variance_ratio_[1]*100:.1f}%)",
+    }
+
+
+    fig = px.scatter(
+        df_plot,
+        x="PC1",
+        y="PC2",
+        color="Tumor-Cancer",
+        labels=labels,
+        hover_name="Patient_ID",
+        title=f"PCA de Subtypes of cancer of mama of {cancer_type_one} and {cancer_type_two}",
+        opacity=0.7,
+        color_discrete_map={f"{cancer_type_one}": "#1f77b4", f"{cancer_type_two}": "#ec3204"},
+    )
+
+    fig.update_traces(marker=dict(size=6))
+    fig.show()
+
+
+
+    
