5440cd42d4e045721de7c0f00b45ef3f02b8f630
ManuelFas <manuelfasruiz@gmail.com>
2026-03-02 09:13:10 +0100

    Added the class of Preprocessor

--- src/utils/Preprocessing.py
+++ src/utils/Preprocessing.py
@@ -2,133 +2,140 @@ import pandas as pd
 from pydeseq2.dds import DeseqDataSet
 from pydeseq2.default_inference import DefaultInference
 
-def classify_cancer_type(df_clinical_data : pd.DataFrame) -> list:
-    """
-    Function to classify between the different type of Cancers,
-    Returns: Luminal A (ER+ HER2-) 
-             Luminal B (ER+ HER2+)
-             HER-enriched (ER- PR- HER2+ )
-             TNBC (ER- PR- HER2- )
-             
-    """
-    res = []
-    
-    for _, row in df_clinical_data.iterrows():
-        Her2_fish_status = row.get("HER2 fish status", pd.NA)
-        Her2_ihc = row.get("HER2 ihc score", pd.NA)
-        ER_status = row.get("ER Status By IHC", pd.NA)
-        PR_status = row.get("PR status by ihc", pd.NA)
+
+
+class Preprocessor:
+    def __init__(self) -> None:
+        pass
+
+
+    def classify_cancer_type(self, df_clinical_data : pd.DataFrame) -> list:
+        """
+        Function to classify between the different type of Cancers,
+        Returns: Luminal A (ER+ HER2-) 
+                Luminal B (ER+ HER2+)
+                HER-enriched (ER- PR- HER2+ )
+                TNBC (ER- PR- HER2- )
+                
+        """
+        res = []
         
-        if pd.notna(Her2_fish_status):
-            if (ER_status == "Positive") and (Her2_fish_status == "Negative"):
-                res.append("Luminal A")
-                
-            elif (ER_status == "Positive") and (Her2_fish_status == "Positive"):
-                res.append("Luminal B")
-                
-            elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_fish_status == "Positive"):
-                res.append("HER2-enriched")
-                
-            elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_fish_status == "Negative"):
-                res.append("TNBC")
+        for _, row in df_clinical_data.iterrows():
+            Her2_fish_status = row.get("HER2 fish status", pd.NA)
+            Her2_ihc = row.get("HER2 ihc score", pd.NA)
+            ER_status = row.get("ER Status By IHC", pd.NA)
+            PR_status = row.get("PR status by ihc", pd.NA)
             
+            if pd.notna(Her2_fish_status):
+                if (ER_status == "Positive") and (Her2_fish_status == "Negative"):
+                    res.append("Luminal A")
+                    
+                elif (ER_status == "Positive") and (Her2_fish_status == "Positive"):
+                    res.append("Luminal B")
+                    
+                elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_fish_status == "Positive"):
+                    res.append("HER2-enriched")
+                    
+                elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_fish_status == "Negative"):
+                    res.append("TNBC")
+                
+                else:
+                    res.append("<UNK>")
+                    
+                continue
+            
+            if pd.isna(Her2_ihc):
+                res.append("<UNK>")
+                
+                continue
+                    
+            if (Her2_ihc == 2):
+                res.append("<UNK>")
+                        
+            elif (ER_status == "Positive") and (Her2_ihc <= 1):
+                res.append("Luminal A")
+                        
+            elif (ER_status == "Positive") and (Her2_ihc >= 3):
+                res.append("Luminal B")
+                    
+            elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_ihc >= 3):
+                res.append("HER2-enriched")
+                    
+            elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_ihc <= 1):
+                res.append("TNBC")
+                    
             else:
                 res.append("<UNK>")
                 
-            continue
+        return res
+
+
+
+    def elimnation_zeros(self, df : pd.DataFrame) -> pd.DataFrame:  
+        genes = df.columns[1:]
         
-        if pd.isna(Her2_ihc):
-            res.append("<UNK>")
-            
-            continue
-                
-        if (Her2_ihc == 2):
-            res.append("<UNK>")
-                    
-        elif (ER_status == "Positive") and (Her2_ihc <= 1):
-            res.append("Luminal A")
-                    
-        elif (ER_status == "Positive") and (Her2_ihc >= 3):
-            res.append("Luminal B")
-                
-        elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_ihc >= 3):
-            res.append("HER2-enriched")
-                
-        elif (ER_status == "Negative") and (PR_status == "Negative") and (Her2_ihc <= 1):
-            res.append("TNBC")
-                
-        else:
-            res.append("<UNK>")
-            
-    return res
+        zeros_genes = (df[genes] == 0).sum(axis=0)
+        
+        max_number_of_zeros = zeros_genes.max()
+        
+        avg_number_of_zeros = zeros_genes.mean()
+        
+        median_number_of_zeros = zeros_genes.median()
+        
+        min_number_of_zeros = zeros_genes.min()
+        
+        print(f"Max of zeros per row in the dataset: {max_number_of_zeros}")
+        
+        print(f"Avg of zeros per row in the dataset: {avg_number_of_zeros}")
+        
+        print(f"Median of zeros per row in the dataset: {median_number_of_zeros}")
+        
+        print(f"Min of zeros per row in the dataset: {min_number_of_zeros}")
+        
+        keep_columns = zeros_genes <= avg_number_of_zeros 
+        
+        df_return = pd.concat(
+            [df[["Tumor-Cancer"]], df.loc[:, genes[keep_columns]]],
+            axis=1
+        )
+        
+        print(f"After the 0 elimination: {df_return.shape[1]}") 
+        
+        return df_return
 
 
-
-def elimnation_zeros(df : pd.DataFrame) -> pd.DataFrame:  
-    genes = df.columns[1:]
-    
-    zeros_genes = (df[genes] == 0).sum(axis=0)
-    
-    max_number_of_zeros = zeros_genes.max()
-    
-    avg_number_of_zeros = zeros_genes.mean()
-    
-    median_number_of_zeros = zeros_genes.median()
-    
-    min_number_of_zeros = zeros_genes.min()
-    
-    print(f"Max of zeros per row in the dataset: {max_number_of_zeros}")
-    
-    print(f"Avg of zeros per row in the dataset: {avg_number_of_zeros}")
-    
-    print(f"Median of zeros per row in the dataset: {median_number_of_zeros}")
-    
-    print(f"Min of zeros per row in the dataset: {min_number_of_zeros}")
-    
-    keep_columns = zeros_genes <= avg_number_of_zeros 
-    
-    df_return = pd.concat(
-        [df[["Tumor-Cancer"]], df.loc[:, genes[keep_columns]]],
-        axis=1
-    )
-    
-    print(f"After the 0 elimination: {df_return.shape[1]}") 
-    
-    return df_return
+    def initialize_DeseqDataSet(self, counts_data : pd.DataFrame, 
+                        metadata:pd.DataFrame, 
+                        design : str) -> DeseqDataSet:
+        """
+        For initializing the DeseqDataSet variable
+        """
+        inference = DefaultInference(n_cpus=2)
+        
+        deseqDataSet = DeseqDataSet(
+            counts=counts_data,
+            metadata=metadata,
+            design=f'~{design}',
+            inference=inference
+        )
+        
+        return deseqDataSet
 
 
-def initialize_DeseqDataSet(counts_data : pd.DataFrame, 
-                     metadata:pd.DataFrame, 
-                     design : str) -> DeseqDataSet:
-    """
-    For initializing the DeseqDataSet variable
-    """
-    inference = DefaultInference(n_cpus=2)
-    
-    deseqDataSet = DeseqDataSet(
-        counts=counts_data,
-        metadata=metadata,
-        design=f'~{design}',
-        inference=inference
-    )
-    
-    return deseqDataSet
+    def total_type_len_type_cancer(self, df: pd.DataFrame) -> list:
+        
+        list_df = self.classify_cancer_type(df_clinical_data=df)
 
+        luminal_A = [x for x in list_df if x == "Luminal A"]
+        luminal_B = [x for x in list_df if x == "Luminal B"]
+        HER2_enriched = [x for x in list_df if x == "HER2-enriched"]
+        TNBC = [x for x in list_df if x == "TNBC"]
+        UNK = [x for x in list_df if x == "<UNK>"]
 
-def total_type_len_type_cancer(df: pd.DataFrame) -> list:
-    
-    list_df = classify_cancer_type(df_clinical_data=df)
+        print(f"Luminal A: {len(luminal_A)} - Total(%): {len(luminal_A) / len(df):.2f}")
+        print(f"Luminal B: {len(luminal_B)} - Total(%):{len(luminal_B) / len(df):.2f}")
+        print(f"HER2-enriched: {len(HER2_enriched)} - Total(%):{len(HER2_enriched) / len(df):.2f}")
+        print(f"TNBC: {len(TNBC)} - Total(%){len(TNBC) / len(df):.2f} ")
+        print(f"UNK: {len(UNK)} - Total(%) {len(UNK) / len(df):.2f}")
 
-    luminal_A = [x for x in list_df if x == "Luminal A"]
-    luminal_B = [x for x in list_df if x == "Luminal B"]
-    HER2_enriched = [x for x in list_df if x == "HER2-enriched"]
-    TNBC = [x for x in list_df if x == "TNBC"]
-    UNK = [x for x in list_df if x == "<UNK>"]
-
-    print(f"Luminal A: {len(luminal_A)} - Total(%): {len(luminal_A) / len(df):.2f}")
-    print(f"Luminal B: {len(luminal_B)} - Total(%):{len(luminal_B) / len(df):.2f}")
-    print(f"HER2-enriched: {len(HER2_enriched)} - Total(%):{len(HER2_enriched) / len(df):.2f}")
-    print(f"TNBC: {len(TNBC)} - Total(%){len(TNBC) / len(df):.2f} ")
-    print(f"UNK: {len(UNK)} - Total(%) {len(UNK) / len(df):.2f}")
-
-    return list_df
+        return list_df
