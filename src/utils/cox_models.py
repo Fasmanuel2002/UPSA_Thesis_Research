6ddec4524b601f7f948885c6ecc33ff15f9fd578
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2026-03-14 15:26:26 +0100

    Added the risk and survival curve for plotting

--- src/utils/cox_models.py
+++ src/utils/cox_models.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 
 def Cox_regression(X_train : pd.DataFrame,
                    Y_train : pd.DataFrame,
-                   X_test : pd.DataFrame, 
+                   X_test : pd.DataFrame,
+                   title: str
                    ) -> Tuple[pd.DataFrame,
                               np.ndarray,
                               np.ndarray,
@@ -32,6 +33,23 @@ def Cox_regression(X_train : pd.DataFrame,
     chp_predict = chp.predict(X_test)
     chp_survival_curve = chp.predict_survival_function(X_test)
     chp_risk_curve = chp.predict_cumulative_hazard_function(X_test)
+    for fn in chp_survival_curve:
+        plt.step(fn.x, fn(fn.x), where="post")
+    plt.title(f"Survival curve for {title}")
+    plt.xlabel("Days")
+    plt.ylim(0, 1)
+    plt.ylabel("% of survival")
+    plt.show()
+    
+    for fn in chp_risk_curve:
+        plt.step(fn.x, fn(fn.x), where="post")
+    plt.title(f"Risk curve for {title}")
+    plt.xlabel("Months")
+    plt.ylim(0, 1)
+    plt.ylabel("% of risk")
+    plt.show()
+    
+    
     return (betas, chp_predict, chp_survival_curve, chp_risk_curve)
 
         
