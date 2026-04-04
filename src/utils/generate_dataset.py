6e373aea83f7ad1b6fda136a7a4f37a5c900fe91
ManuelFas <manuelfasruiz@gmail.com>
2026-04-04 10:05:22 +0200

    Added the TorchPreprocessing

---
+++ src/utils/generate_dataset.py
@@ -0,0 +1,102 @@
+
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from src.utils.Preprocessing import Preprocessor
+from sksurv.util import Surv
+from numpy import ndarray
+from sklearn.preprocessing import StandardScaler
+
+genes_expression = [
+    "TBC1D9",
+    "SUSD3",
+    "SLC39A6",
+    "GFRA1",
+    "SOX11",
+    "GATA3",
+    "SLC15A2",
+    "NANOS1",
+    "ZNF552",
+    "ESR1",
+    "NAT1",
+    "NME3",
+    "DNALI1",
+    "AGR3",
+    "CA12",
+    "BCL2",
+    "MKI67",
+    "TP53",
+    "AURKA"
+]
+
+class TorchPreprocessing:
+    def __init__(self, df_mrna_clean : pd.DataFrame, df_clinical_data_clean : pd.DataFrame) -> None:
+        self.pp = Preprocessor()
+        self.genes_expression = genes_expression
+        self.df_mrna_clean = df_mrna_clean
+        self.df_clinical_data_clean = df_clinical_data_clean
+        
+    def get_expression_cols(self) -> pd.DataFrame:
+        expressions_genes_cols = pd.DataFrame()
+        for index, gene in enumerate(self.genes_expression):
+            df_genes = self.pp.gene_to_long(self.df_mrna_clean, gene)
+            if index == 0:
+                expressions_genes_cols = (df_genes[["Sample ID", "expression"]]
+                                         .rename(columns={"expression": gene}).copy())
+            else: 
+                expressions_genes_cols = expressions_genes_cols.merge(
+                df_genes[["Sample ID", "expression"]].rename(columns={"expression": gene}),
+                on="Sample ID",
+                how="inner"
+            )      
+        return expressions_genes_cols
+    
+    def get_comparation_df(self) -> pd.DataFrame:
+        expression_genes_cols = self.get_expression_cols()
+        df_merged = pd.merge(
+            left=self.df_clinical_data_clean, 
+            right=expression_genes_cols,
+            left_on="Sample ID",
+            right_on="Sample ID",
+        )
+        
+        cols = ["Sample ID","Tumor-Cancer", "Overall Survival Status", "Overall Survival (Months)"] + self.genes_expression
+        comparation_df = df_merged.loc[
+            df_merged["Tumor-Cancer"].isin(["Luminal A", "Luminal B", "TNBC", "HER2-enriched"]),
+            cols
+        ]
+        return self.pp.eliminate_zero_genes(comparation_df, "Tumor-Cancer", threshold=0.8)
+    
+    
+    def get_data_set(self) -> Tuple:
+        comparation_df = self.get_comparation_df()
+        status = comparation_df["Overall Survival Status"].astype(str).str.strip()
+        
+        comparation_df["event"] = status.str.contains("DECEASED", na=False)
+        
+        comparation_df["event_60"] = comparation_df["event"].copy()
+        
+        comparation_df["time_60"] =  np.minimum(comparation_df["Overall Survival (Months)"], 60)
+        comparation_df.loc[
+            comparation_df["Overall Survival (Months)"] > 60, "event_60"
+        ] = False
+
+        comparation_df = comparation_df.dropna(subset=["time_60"]).copy()
+        
+        comparation_df = comparation_df.drop(columns="Sample ID")
+
+
+        X = comparation_df.loc[:, self.genes_expression].astype(float)
+        
+        X_log = np.log2(X + 1)
+        
+        scaler = StandardScaler()
+        
+        X_scaled = scaler.fit_transform(X_log)
+        
+        durations = comparation_df["time_60"].values
+        
+        events = comparation_df["event_60"].values.astype(float)
+        
+        return X_scaled, durations, events, scaler
